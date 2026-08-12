132d38d4de0d3aa76b4528dc5d17e62199177f1f
Moses <mcam589@wgu.edu>
2026-08-12 17:28:35 -0600

    Wired up ChapterView component with Chapter data, first successful render on site

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChapterView } from './chapter-view/chapter-view';
+import { CHAPTERS } from './chapter.data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChapterView],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('SS-Site');
+  protected readonly title = signal('todo-app');
+  protected readonly firstChapter = CHAPTERS[0];
 }
